57bf87977ccc53d8760a996b139907fa6c8425ba
salinawu <salinawu@uchicago.edu>
2015-12-26 00:34:18 -0500

    updating/showing/deleting users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225042806) do
+ActiveRecord::Schema.define(version: 20151226052238) do
 
   create_table "postings", force: :cascade do |t|
     t.string   "location"
@@ -25,10 +25,11 @@ ActiveRecord::Schema.define(version: 20151225042806) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
